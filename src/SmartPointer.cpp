e7ab55d96874b6b4fa108fcf72bac7b77607e557
zpye <986790855@qq.com>
2018-09-16 09:55:14 +0800

    update smart pointer

--- src/SmartPointer.cpp
+++ src/SmartPointer.cpp
@@ -6,5 +6,38 @@ using namespace std;
 
 void test_smartpointer()
 {
+    cout << "==========simple shared pointer==========" << endl;
+    {
+        shared_ptr< S > s1(new S());
+        shared_ptr< S > s2 = make_shared< S >();
 
+        {
+            shared_ptr< S > s3(s1);
+        }
+
+        {
+            shared_ptr< S > s3 = s1;
+        }
+
+        {
+            shared_ptr< S > s3 = make_shared< S >();
+            s3.reset(new S());
+        }
+
+        {
+            shared_ptr< S > s4 = move(s2);
+        }
+
+        cout << "simple shared pointer end" << endl;
+    }
+
+    cout << "==========unique pointer==========" << endl;
+    {
+        unique_ptr< S > u1(new S());
+        // unique_ptr< S > u2 = u1;
+        unique_ptr< S > u2 = move(u1);
+        unique_ptr< S > u3(move(u2));
+
+        cout << "unique pointer end" << endl;
+    }
 }
--- src/SmartPointer.h
+++ src/SmartPointer.h
@@ -1,6 +1,27 @@
 #ifndef CXX_PRACTICE_SMART_POINTER_H
 #define CXX_PRACTICE_SMART_POINTER_H
 
+#include <iostream>
 
+class S
+{
+public:
+    S()
+    {
+        cnt = count++;
+        std::cout << "S() " << cnt << std::endl;
+    }
+
+    ~S()
+    {
+        std::cout << "~S() " << cnt << std::endl;
+    }
+
+protected:
+    static size_t count;
+    size_t cnt;
+};
+
+size_t S::count = 0;
 
 #endif // CXX_PRACTICE_SMART_POINTER_H
