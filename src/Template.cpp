99e33d015113a6a022d64eaf7ca25f4001bc44b7
zpye <986790855@qq.com>
2018-09-18 00:45:50 +0800

    update template

--- src/Template.cpp
+++ src/Template.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+template< typename F, typename T >
+auto template_func(F f, T t)
+    ->decltype(f(t))
+{
+    return f(t);
+}
+
 void test_template()
 {
     cout << "==========template function==========" << endl;
@@ -37,5 +44,21 @@ void test_template()
         cout << "value: " << TempStruct< 0 >::value << endl;
         cout << "value: " << TempStruct< 4 >::value << endl;
         cout << "value: " << TempStruct< 10 >::value << endl;
+    }
+
+    cout << "==========auto==========" << endl;
+    {
+        auto i1 = 1; // int
+        auto i2 = 5 + 6; //int
+        auto* i3 = &i1; // int *
+        auto& i4 = i2; // int &
+        const auto& i5 = 10; // const int &
+
+        auto i6 = i3; // int *
+        auto i7 = i4; // int
+        auto i8 = i5; // int
+
+        cout << "template_func: " 
+            << template_func([](double t) { return 3 * t; }, 1.2);
     }
 }
